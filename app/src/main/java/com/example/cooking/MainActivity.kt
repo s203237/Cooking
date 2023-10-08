16e449086584f81199e65ea2cc2f7463cbb14c01
Camellia <91070768+s203237@users.noreply.github.com>
2023-10-08 20:25:15 +0200

    update

--- app/src/main/java/com/example/cooking/MainActivity.kt
+++ app/src/main/java/com/example/cooking/MainActivity.kt
@@ -10,12 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.cooking.UI.RecipeList.RecipeList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+RecipeList()
         }
     }
 }
--- app/src/main/java/com/example/cooking/UI/RecipeList/RecipeList.kt
+++ app/src/main/java/com/example/cooking/UI/RecipeList/RecipeList.kt
@@ -15,9 +15,73 @@
  */
 package com.example.cooking.UI.RecipeList
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cooking.Data.Recipe
+import com.example.cooking.R
 
+@Composable
+fun RecipeList(recipes: List<Recipe> = testRecipes) {
+    Column  {
 
+        Text(
+            text = "Recipe List",
+            textAlign = TextAlign.Left,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
 
+        )
+        LazyColumn {
+            items(recipes) { recipe ->
+                RecipeItem(recipe)
+
+            }
+        }
+    }
+}
+@Composable
+fun RecipeItem(recipe: Recipe, index: Int =0){
+
+Column(
+    modifier = Modifier.fillMaxWidth().padding(10.dp)
+){
+    Image(painter = painterResource(id= recipe.imageResourceId),
+        contentDescription = recipe.name,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.height(height = 170.dp).fillMaxWidth(),
+        )
+    Text(
+        text = recipe.name ,
+        fontSize = 20.sp,
+        modifier = Modifier.padding(10.dp)
+    )
+}
+
+}
+val testRecipes = listOf(
+    Recipe("test 1", R.drawable.image),
+    Recipe("test 2", R.drawable.image2),
+    Recipe("test 3", R.drawable.images3),
+    Recipe("test 4", R.drawable.image4),
+
+)
 
 
