0564bc4e2853857ec7c07c3b0d49fd1e02d25da4
Melissa Woo <s224311@dtu.dk>
2023-10-11 01:30:22 +0200

    Adding Heading function to formaat heading

--- app/src/main/java/com/example/cooking/UI/sharedComponents/Formatting.kt
+++ app/src/main/java/com/example/cooking/UI/sharedComponents/Formatting.kt
@@ -1,4 +1,22 @@
 package com.example.cooking.UI.sharedComponents
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 class Formatting {
+    @Composable
+    fun Heading(heading: String) {
+        Text(
+            text = heading.uppercase(),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+    }
+
 }
