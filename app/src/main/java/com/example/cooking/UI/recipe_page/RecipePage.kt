e1d57b6ecb94422a477f39b4c826e8ca0cd48408
Melissa Woo <s224311@dtu.dk>
2023-10-11 02:20:14 +0200

    removing unused import statements

--- app/src/main/java/com/example/cooking/UI/recipe_page/RecipePage.kt
+++ app/src/main/java/com/example/cooking/UI/recipe_page/RecipePage.kt
@@ -1,13 +1,10 @@
 package com.example.cooking.UI.recipe_page
-
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -15,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
