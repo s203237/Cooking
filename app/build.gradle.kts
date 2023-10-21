22f5f01a3d0f05402dfdc6e4e794d94db8910660
Clubfuzzbutt <99827391+Clubfuzzbutt@users.noreply.github.com>
2023-10-21 11:18:40 +0200

    Profile Page updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.cooking"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cooking"
@@ -48,15 +48,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
     testImplementation("junit:junit:4.13.2")
+    implementation("io.coil-kt:coil-compose:2.4.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- app/src/main/java/com/example/cooking/MainActivity.kt
+++ app/src/main/java/com/example/cooking/MainActivity.kt
@@ -11,6 +11,47 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cooking.UI.RecipeList.RecipeList
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import coil.compose.rememberAsyncImagePainter
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +61,274 @@ RecipeList()
         }
     }
 }
+
+@Composable
+fun MyPrivacy() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .absolutePadding(50.dp, 200.dp, 10.dp, 220.dp)
+            .height(200.dp)
+            .width(200.dp)
+    ){
+        Button(
+            modifier = Modifier.align(Alignment.CenterStart),
+            onClick = {}
+        ) {
+            Text("Privacy", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Composable
+fun MyHelp() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .absolutePadding(50.dp, 200.dp, 10.dp, 80.dp)
+            .height(200.dp)
+            .width(200.dp)
+    ){
+        Button(
+            modifier = Modifier.align(Alignment.CenterStart),
+            onClick = {}
+        ) {
+            Text("Help", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen(){
+
+
+
+
+    val notification = rememberSaveable{mutableStateOf("") }
+    if (notification.value.isNotEmpty()) {
+        Toast.makeText(LocalContext.current, notification.value, Toast.LENGTH_LONG).show()
+        notification.value = ""
+    }
+
+    var name by rememberSaveable { mutableStateOf("Default Name")}
+    var email by rememberSaveable { mutableStateOf("Default Email")}
+
+
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .padding(8.dp)){
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Account Settings", fontSize = 20.sp)
+            Text(text = "Logout", modifier = Modifier.clickable { notification.value = "You logged out"  })
+
+
+        }
+
+        ProfileImage()
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp), verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(text = "Name", modifier = Modifier.width(100.dp))
+            TextField(value = name, onValueChange = {name = it} )
+        }
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp), verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(text = "Email", modifier = Modifier.width(100.dp))
+            TextField(value = email, onValueChange = {email = it} )
+        }
+
+    }
+
+}
+
+@Composable
+fun ProfileImage() {
+    val imageUri = rememberSaveable { mutableStateOf("") }
+    val painter = rememberAsyncImagePainter(
+        if (imageUri.value.isEmpty())
+            R.drawable.profilefic
+        else
+            imageUri.value
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ){ uri: Uri? ->
+        uri?.let{ imageUri.value = it.toString() }
+
+
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(100.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable { launcher.launch("image/*") },
+                contentScale = ContentScale.Crop)
+
+        }
+        Text(text = "Change Profile Picture")
+    }
+}
+
+@Composable
+fun CommentImage() {
+    val imageUri = rememberSaveable { mutableStateOf("") }
+    val painter = rememberAsyncImagePainter(
+        if (imageUri.value.isEmpty())
+            R.drawable.create
+        else
+            R.drawable.baseline_comment_24
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ){ uri: Uri? ->
+        uri?.let{ imageUri.value = it.toString() }
+
+
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(80.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Card(
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(50.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable { launcher.launch("image/*") },
+                contentScale = ContentScale.Crop)
+
+        }
+        Text(text = "Created")
+    }
+}
+
+@Composable
+fun FavouriteImage() {
+    val imageUri = rememberSaveable { mutableStateOf("") }
+    val painter = rememberAsyncImagePainter(
+        if (imageUri.value.isEmpty())
+            R.drawable.baseline_comment_24
+        else
+            imageUri.value
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ){ uri: Uri? ->
+        uri?.let{ imageUri.value = it.toString() }
+
+
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(80.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Card(
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(50.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable { launcher.launch("image/*") },
+                contentScale = ContentScale.Crop)
+
+        }
+        Text(text = "Comments")
+    }
+}
+
+@Composable
+fun CreateImage() {
+    val imageUri = rememberSaveable { mutableStateOf("") }
+    val painter = rememberAsyncImagePainter(
+        if (imageUri.value.isEmpty())
+            R.drawable.favourite
+        else
+            imageUri.value
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ){ uri: Uri? ->
+        uri?.let{ imageUri.value = it.toString() }
+
+
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(80.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Card(
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(50.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable { launcher.launch("image/*") },
+                contentScale = ContentScale.Crop)
+
+        }
+        Text(text = "Favourite")
+    }
+}
+
+
+
+
+
 
 
