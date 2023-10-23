35811aca9431180e0005a4cfedab1b6f246b16e0
Clubfuzzbutt <99827391+Clubfuzzbutt@users.noreply.github.com>
2023-10-23 09:58:53 +0200

    Still missing comment

--- app/src/main/java/com/example/cooking/UI/Login/Profile.kt
+++ app/src/main/java/com/example/cooking/UI/Login/Profile.kt
@@ -1,35 +1,21 @@
-package com.example.cooking
+package com.example.cooking.UI.Login
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import android.net.Uri
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
@@ -37,36 +23,27 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
+import com.example.cooking.R
 
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ProfileScreen()
-            MyHelp()
-            MyPrivacy()
-            CommentImage()
-            FavouriteImage()
-            CreateImage()
-                }
-
-            }
-
-        }
 
 @Composable
 fun MyPrivacy() {
@@ -113,14 +90,14 @@ fun ProfileScreen(){
 
 
 
-    val notification = rememberSaveable{mutableStateOf("") }
+    val notification = rememberSaveable{ mutableStateOf("") }
     if (notification.value.isNotEmpty()) {
         Toast.makeText(LocalContext.current, notification.value, Toast.LENGTH_LONG).show()
         notification.value = ""
     }
 
-    var name by rememberSaveable { mutableStateOf("Default Name")}
-    var email by rememberSaveable { mutableStateOf("Default Email")}
+    var name by rememberSaveable { mutableStateOf("Default Name") }
+    var email by rememberSaveable { mutableStateOf("Default Email") }
 
 
     Column(modifier = Modifier
@@ -162,9 +139,9 @@ fun ProfileScreen(){
         Row(
             modifier = Modifier. fillMaxWidth().padding(8.dp)
         ){
-        CommentImage()
-        CreateImage()
-        FavouriteImage()
+            CommentImage()
+            CreateImage()
+            FavouriteImage()
         }
     }
 
@@ -240,7 +217,7 @@ fun CommentImage() {
         modifier = Modifier
             //.padding(80.dp)
             .fillMaxWidth(0.33f),
-       // horizontalAlignment = Alignment.CenterHorizontally,
+        // horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom
     ) {
         Card(
@@ -347,11 +324,3 @@ fun CreateImage() {
         Text(text = "Favourite")
     }
 }
-
-
-
-
-
-
-
-
