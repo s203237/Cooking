2c38ad9764638055f92df627e1dce5a76e12fffc
Camellia <91070768+s203237@users.noreply.github.com>
2023-10-23 09:32:26 +0200

    About us page

--- app/src/main/java/com/example/cooking/MainActivity.kt
+++ app/src/main/java/com/example/cooking/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
            // AccountCreationPage()
            //OnBoardingPage()
             //RecipeList()
+            //scrollableList()
            AboutUsPage()
         }
     }
