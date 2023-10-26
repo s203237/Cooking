7c3bcea8162da54ba292d20268128f4db0ded7d1
Melissa Woo <s224311@dtu.dk>
2023-10-26 14:39:14 +0200

    merging in updated search package

--- app/src/main/java/com/example/cooking/UI/Search/SearchActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.cooking.UI.Search
-
-class SearchActivity {
-}
--- app/src/main/java/com/example/cooking/UI/Search/SearchPage.kt
+++ app/src/main/java/com/example/cooking/UI/Search/SearchPage.kt
@@ -1,44 +1,67 @@
 package com.example.cooking.UI.Search
+import android.app.Activity
+import android.os.Build
+import android.widget.ImageButton
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.cooking.R
+import com.example.cooking.UI.Homepage.Homepage
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun SearchPage() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .background(color = Color(0xffF2ECE3))
             .fillMaxWidth()
             .fillMaxHeight()
-            .padding(40.dp)
+            .padding(30.dp)
     ) {
+Row(){
+    IconButton(onClick = { /*BackHomePage()*/ }) {
+        Icon(painter = painterResource(id = R.drawable.arrow_back),
+            contentDescription ="back to homepage"
+        )
+    }
+    Spacer(modifier = Modifier.width(30.dp))
         Text(
             modifier = Modifier.height(55.dp),
             text = "vegelicious",
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
-        )
+            
+        )}
 
         OutlinedTextField(
             value = "What would you like to cook?",
@@ -46,66 +69,73 @@ fun SearchPage() {
             shape = RoundedCornerShape(40)
         )
         Column() {
-            Row() {
-                CreateButton(color = Color(0xffB8C75E), text = "Dinner")
-                CreateButton(color = Color(0xffFF614B), text = "Dessert")
+            FlowRow( maxItemsInEachRow = 2) {
+                Box( modifier =Modifier.weight(1f)){
+                    CreateButton(color = Color(0xffB8C75E), text = "Dinner")}
+                Box( modifier =Modifier.weight(1f)){
+                    CreateButton(color = Color(0xffFF614B), text = "   Dessert  ")
+                }
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xff93BBCF), text = "Sides")}
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xffF3B144), text = "Breakfast")}
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xffF3B144), text = "Healthy")}
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xffB8C75E), text = "Easy")}
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xffFF614B), text = "Salad")}
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xff93BBCF), text = "Pasta")}
             }
-            Row() {
-                CreateButton(color = Color(0xff93BBCF), text = "Sides")
-                CreateButton(color = Color(0xffF3B144), text = "Breakfast")
-            }
-            Row() {
-                CreateButton(color = Color(0xffF3B144), text = "Healthy")
-                CreateButton(color = Color(0xffB8C75E), text = "Easy")
-            }
-            Row() {
-                CreateButton(color = Color(0xffFF614B), text = "Salad")
-                CreateButton(color = Color(0xff93BBCF), text = "Pasta")
-            }
+
+
         }
         Text(
             modifier = Modifier
-                .align(Alignment.Start).padding(10.dp, 10.dp),
+                .align(Alignment.Start)
+                .padding(10.dp, 10.dp),
             text = "Cooking Time",
             fontSize = 25.sp,
             fontWeight = FontWeight.Normal
         )
         Column {
-            Row() {
-                CreateButton(color = Color(0xff93BBCF), text = "Under 30 Min")
-                CreateButton(color = Color(0xffFF614B), text = "Under 45 Min")
-            }
-
-            Row() {
-                CreateButton(color = Color(0xffB8C75E), text = "Under 1 hour")
-                CreateButton(color = Color(0xffF3B144), text = "1 + hours")
+            FlowRow(modifier = Modifier.fillMaxWidth(), maxItemsInEachRow = 2) {
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xff93BBCF), text = "Under 30 Min")}
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xffFF614B), text = "Under 45 Min")}
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xffB8C75E), text = "Under 1 hour")}
+                Box( modifier =Modifier.weight(1f)){
+                CreateButton(color = Color(0xffF3B144), text = "1 + hours")}
 
             }
         }
     }
 }
+
 @Composable
 fun CreateButton(color:Color, text: String){
     Button(
         onClick = {},
        modifier = Modifier
-            .padding(horizontal = 10.dp, vertical = 10.dp),
+           .fillMaxWidth()
+           .padding(horizontal = 10.dp, vertical = 10.dp)
+           ,
 
         colors = ButtonDefaults.buttonColors(
             containerColor = color,
             contentColor = Color.Black,
         ),
         shape = RoundedCornerShape(40),
-
         ) {
         Text(
             text = text,
-
             )
-
     }
-
 }
+
 
 @Preview
 @Composable
