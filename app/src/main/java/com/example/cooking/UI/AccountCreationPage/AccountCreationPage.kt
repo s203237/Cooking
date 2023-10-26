457a2100ddd2f9f62424922a53d4c07b21f4c42d
Melissa Woo <s224311@dtu.dk>
2023-10-26 14:43:18 +0200

    merging in my work from the working temp main branch

--- app/src/main/java/com/example/cooking/UI/AccountCreationPage/AccountCreationPage.kt
+++ app/src/main/java/com/example/cooking/UI/AccountCreationPage/AccountCreationPage.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,12 +19,12 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -35,103 +36,117 @@ import com.example.cooking.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AccountCreationPage() {
+fun AccountCreationPage(onNavigateToHomeScreen: () -> Unit) {
     Box(
-
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color(0xFFB8C75E))
+            .background(color = Color(0xFFB8C75E)),
+        contentAlignment = Alignment.TopCenter
     ) {
-
-        Image(
-            painter = painterResource(id = R.drawable.apple),
-            contentDescription = "apple icon",
+        Column (
             modifier = Modifier
-                .size(180.dp)
-                .offset(180.dp, 620.dp)
-
-        )
-        Image(
-            painter = painterResource(id = R.drawable.turnip),
-            contentDescription = "turnip icon",
-            modifier = Modifier
-                .size(250.dp)
-                .offset(15.dp, 550.dp)
-                .rotate(5f)
-        )
-        Column(
-            horizontalAlignment = Alignment.Start,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(45.dp)
+                .fillMaxWidth(0.75f)
         ) {
 
-            Spacer(modifier = Modifier.height(60.dp))
+            Spacer(modifier = Modifier.height(70.dp))
 
             Text(
                 text = "Let's create an account so that you can save your favourite recipes, plan your meals and much more!",
-                textAlign = TextAlign.Left,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Normal
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                modifier = Modifier
+                    .padding(
+                        start = 8.dp,
+                        end = 8.dp
+                    )
             )
-            Spacer(modifier = Modifier.height(30.dp))
+
+            Spacer(modifier = Modifier.height(16.dp))
+
             Text(
-                modifier = Modifier.padding(horizontal = 10.dp, vertical = 4.dp),
                 text = "name:",
-                textAlign = TextAlign.Left,
                 fontSize = 40.sp,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 8.dp)
             )
-            OutlinedTextField(
+            TextField(
                 value = "Please enter your name!",
                 onValueChange = {},
-                shape = RoundedCornerShape(40),
-                modifier = Modifier
-                    .background(Color.White)
-                    .fillMaxWidth(),
-            )
-            Text(
-                modifier = Modifier.padding(horizontal = 10.dp, vertical = 4.dp),
-                text = "e-mail:",
-                textAlign = TextAlign.Left,
-                fontSize = 40.sp,
-                fontWeight = FontWeight.Bold
-            )
-            OutlinedTextField(
-                value = "Please enter your e-mail!",
-                onValueChange = {},
-                shape = RoundedCornerShape(40),
+                shape = RoundedCornerShape(60),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color(0xFFF2ECE3),
+                    textColor = Color.Black
+                ),
                 modifier = Modifier
                     .fillMaxWidth()
-
+                    .height(50.dp),
             )
-            Spacer(modifier = Modifier.height(40.dp))
-            Button(
-                onClick = {},
-                // Spacer(modifier = Modifier.height(50.dp))
-                modifier = Modifier//.padding(horizontal = 15.dp, vertical = 60.dp)
-                    .align(Alignment.End),
-                colors = ButtonDefaults.buttonColors(
 
-                    containerColor = Color.White,
-                    contentColor = Color.Red,
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "e-mail:",
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+            )
+            TextField(
+                value = "Please enter your e-mail!",
+                onValueChange = {},
+                shape = RoundedCornerShape(60),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color(0xFFF2ECE3),
+                    textColor = Color.Black
                 ),
-                shape = RoundedCornerShape(40),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+            )
 
-                ) {
-                Text(
-                    text = "submit",
+            Spacer(modifier = Modifier.height(22.dp))
 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Button(
+                    onClick = onNavigateToHomeScreen,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFF2ECE3),
+                        contentColor = Color.Red
                     )
-
-
+                ) {
+                    Text(
+                        text = "submit",
+                        fontSize = 16.sp
+                    )
+                }
             }
-        }
+            Box(){
+                Image(
+                    painter = painterResource(id = R.drawable.apple),
+                    contentDescription ="apple vector graphic",
+                    modifier = Modifier
+                        .size(180.dp)
+                        .offset(110.dp, 75.dp)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.turnip),
+                    contentDescription ="turnip vector graphic",
+                    modifier = Modifier
+                        .size(230.dp)
+                        .offset((-50).dp, 10.dp)
+                )
+            }
 
+        }
     }
 }
 
 @Preview
 @Composable
-fun PreviewAccountCreation() { AccountCreationPage()
+fun PreviewAccountCreation() { AccountCreationPage(onNavigateToHomeScreen = {})
 }
