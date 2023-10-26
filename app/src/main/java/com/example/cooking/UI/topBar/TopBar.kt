99745c40e0caf734de5d1640493385b234deba4e
Bayan <113907779+BayanDowairi@users.noreply.github.com>
2023-10-26 22:46:00 +0200

    Drop Down menu created

--- app/src/main/java/com/example/cooking/UI/topBar/TopBar.kt
+++ app/src/main/java/com/example/cooking/UI/topBar/TopBar.kt
@@ -2,17 +2,24 @@ package com.example.cooking.UI.topBar
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,6 +29,13 @@ import androidx.compose.ui.unit.sp
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTopBar() {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf("") }
+    val menuItems = listOf(
+        "Dinner", "Breakfast", "Lunch", "Dessert", "Snacks", "Soup",
+        "Vegan", "One-Pot Meal", "High Protein", "Under 30 min", "Weeknight Dinner",
+        "Appetizers", "Seasonal"
+    )
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -51,93 +65,33 @@ fun MyTopBar() {
                 }
             )
         }
-    ){values -> 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(values)
-        ){
-            item{
-                Text(
-                    text ="Dinner",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Breakfast",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Lunch",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Dessert",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Snacks",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Soup",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Vegan",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="One-Pot Meal",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="High Protein",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Under 30 min",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Weeknight Dinner",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Appetizers",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-            item{
-                Text(
-                    text ="Seasonal",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
+    ){paddingValues ->
+        Modifier.padding(paddingValues)
+        TextField(
+            value =selectedItem ,
+            onValueChange = {
+                selectedItem=it
+                expanded=true
+            }, singleLine = true,
+            label={ Text(text = "menu")},
+            modifier=Modifier.padding(16.dp)
+        )
+        if(expanded){
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded=false },
+            modifier = Modifier.size(200.dp)
+        ) {menuItems.forEach{item ->
+            DropdownMenuItem(
+                text = {Text(item)},
+                onClick = {  selectedItem = item
+                expanded = false
+                }
+            )
+          }
         }
-
-    }
+     }
+   }
 }
 @Preview
 @Composable
