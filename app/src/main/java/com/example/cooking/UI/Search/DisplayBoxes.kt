9cba3408047fd8fa8d10c82e75ee24f980ab468c
NegarOstad <s224283@student.dtu.dk>
2023-10-27 20:30:25 +0200

    change the colors of the boxes

--- app/src/main/java/com/example/cooking/UI/Search/DisplayBoxes.kt
+++ app/src/main/java/com/example/cooking/UI/Search/DisplayBoxes.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,32 +17,41 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun  DisplayTextBoxes(){
+fun  DisplayTextBoxes() {
     val categoriesList = CategoriesList()
+    val colorList = ColorList()
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        categoriesList.forEach { category ->
-            Box(
+        categoriesList.forEachIndexed { index, category ->
+            val color = colorList.getOrNull(index)
+            if (color != null) {
+                val backgroundColor = Color(android.graphics.Color.parseColor(color))
 
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp)
-                    .background(Color.LightGray)
-                    .border(1.dp, Color.Gray, RoundedCornerShape(50.dp))
+                Box(
 
-            ) {
-                Text(
-                    text = category,
-                    modifier = Modifier.padding(16.dp),
-                    fontSize = 18.sp,
-                    color = Color.Black
-                )
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                        .background(backgroundColor)
+                        .border(1.dp, Color.Gray, RoundedCornerShape(50.dp))
+
+                ) {
+                    Text(
+                        text = category,
+                        modifier = Modifier.padding(16.dp),
+                        fontSize = 18.sp,
+                        color = Color.Black
+                    )
+                }
             }
+
         }
-
     }
-
 }
+
+
+
+
