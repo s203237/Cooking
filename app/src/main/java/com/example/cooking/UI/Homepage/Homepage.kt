3ffaace68b0c4cbe81b8fc9890b47e47d7ffac68
Magnus Ottosen <magnus@ottosen.dk>
2023-10-30 11:54:36 +0100

    Update Homepage.kt

--- app/src/main/java/com/example/cooking/UI/Homepage/Homepage.kt
+++ app/src/main/java/com/example/cooking/UI/Homepage/Homepage.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,12 +68,13 @@ fun scrollableList(
                 Text(
                     text = "test 1",
                     fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(10.dp)
                 )
                 Text(
                     text = "View more", // Need to be button sending user to category
                     fontSize = 20.sp,
-                    modifier = Modifier.padding(10.dp),
+                    modifier = Modifier.padding(5.dp),
                     color = Color.Blue
                 )
                 LazyRow {
