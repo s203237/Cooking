612fc06b0bb9e91b8096e4d23a2c10c2edbf0f68
Magnus Ottosen <magnus@ottosen.dk>
2023-10-30 12:17:26 +0100

    Fixed stuff

--- app/src/main/java/com/example/cooking/UI/Login/Profile.kt
+++ app/src/main/java/com/example/cooking/UI/Login/Profile.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
+//import coil.compose.rememberAsyncImagePainter
 import com.example.cooking.R
 
 
