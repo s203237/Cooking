e4bb200da54407ec00d45c2c0016d05b5a8a44d5
Melissa Woo <s224311@dtu.dk>
2023-11-05 12:16:39 +0100

    correcting syntax of json strings

--- app/src/main/java/com/example/cooking/data/remote/MockRecipeCardDataSource.kt
+++ app/src/main/java/com/example/cooking/data/remote/MockRecipeCardDataSource.kt
@@ -2,9 +2,11 @@ package com.example.cooking.data.remote
 
 class MockRecipeCardDataSource: RecipeDataSource {
     override suspend fun fetchRecipes(): List<String> = listOf(
-        "{\"recipeId\":\"One-pot paneer curry pie\",\"name\":\"One-pot paneer curry pie\",\"imageSrc\":\"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/one-pot-paneer-curry-pie-c1bcd2f.jpg\"",
-        "{\"recipeId\":\"Autumn vegetable salad with saffron dressing\",\"name\":\"Autumn vegetable salad with saffron dressing\",\"imageSrc\":\"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/autumn-vegetable-salad-with-saffron-dressing-ff72d14.jpg\"",
-        "{\"recipeId\":\"Miso & butternut soup\",\"name\":\"Miso & butternut soup\",\"imageSrc\":\"https://images.immediate.co.uk/production/volatile/sites/30/2021/09/Miso-and-butternut-soup-efe9277.jpg\""
+        "{\"recipeId\":\"One-pot paneer curry pie\",\"name\":\"One-pot paneer curry pie\",\"imageSrc\":\"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/one-pot-paneer-curry-pie-c1bcd2f.jpg\"}",
+
+        //"{\"recipeId\":\"One-pot paneer curry pie\",\"name\":\"One-pot paneer curry pie\",\"imageSrc\":\"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/one-pot-paneer-curry-pie-c1bcd2f.jpg\"}",
+        "{\"recipeId\":\"Autumn vegetable salad with saffron dressing\",\"name\":\"Autumn vegetable salad with saffron dressing\",\"imageSrc\":\"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/autumn-vegetable-salad-with-saffron-dressing-ff72d14.jpg\"}",
+        "{\"recipeId\":\"Miso & butternut soup\",\"name\":\"Miso & butternut soup\",\"imageSrc\":\"https://images.immediate.co.uk/production/volatile/sites/30/2021/09/Miso-and-butternut-soup-efe9277.jpg\"}"
 
     )
 }
