f5f30aa91862761dd70d019c64d7edc82f14c6f6
Melissa Woo <s224311@dtu.dk>
2023-11-05 11:49:16 +0100

    Changing field name of image url

--- app/src/main/java/com/example/cooking/data/remote/RecipeCard.kt
+++ app/src/main/java/com/example/cooking/data/remote/RecipeCard.kt
@@ -1,3 +1,14 @@
 package com.example.cooking.data.remote
 
-data class RecipeCard()
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RecipeCard(
+    val recipeId: String,
+    @SerialName("name")
+    val title: String,
+    @SerialName("imageSrc")
+    val imageUrl: String,
+    val imageDescription: String,
+)
