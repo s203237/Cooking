f4dead177e4af52486dd9fcbdc56341a08c5e4e5
Melissa Woo <s224311@dtu.dk>
2023-11-05 23:41:10 +0100

    loading vegetarian recipes collection

--- app/src/main/java/com/example/cooking/data/remote/ApiService.kt
+++ app/src/main/java/com/example/cooking/data/remote/ApiService.kt
@@ -4,7 +4,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
-    @GET("collection/breakfast-recipes/recipes/?rapidapi-key=564d89b1demshec6f0e210cbdd20p1c252ajsnafddd033c068")
+    @GET("collection/autumn-vegetarian-recipes/recipes/?rapidapi-key=564d89b1demshec6f0e210cbdd20p1c252ajsnafddd033c068")
     suspend fun fetchRecipeCollection(): RecipeCollection
 
     @GET("recipe/{recipeId}/?rapidapi-key=564d89b1demshec6f0e210cbdd20p1c252ajsnafddd033c068")
