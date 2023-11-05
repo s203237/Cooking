436ab0524fbb506a18bad64b90953dcf4e4c586e
Melissa Woo <s224311@dtu.dk>
2023-11-05 11:48:45 +0100

    deserializing strings in for loop and returning object list

--- app/src/main/java/com/example/cooking/data/remote/Deserialize.kt
+++ app/src/main/java/com/example/cooking/data/remote/Deserialize.kt
@@ -3,6 +3,19 @@ import com.example.cooking.model.Recipe
 import kotlinx.serialization.json.Json
 
 fun getRecipeObjects(recipesListJson: List<String>): List<Recipe> {
-    val recipeString = recipesListJson.joinToString() //TODO figure out how not to have to make it a single string
-    return Json.decodeFromString(recipeString)
+    //val recipeString = recipesListJson.joinToString() //TODO figure out how not to have to make it a single string
+    //return Json.decodeFromString(recipeString)
+    val recipes = mutableListOf<Recipe>()
+    for(recipe in recipesListJson)
+        recipes.add(Json.decodeFromString(recipe))
+    return recipes
+}
+
+fun getRecipeCardObjects(recipesListJson: List<String>): List<RecipeCard> {
+    //val recipeString = recipesListJson.joinToString() //TODO figure out how not to have to make it a single string
+    //return Json.decodeFromString(recipeString)
+    val recipeCards = mutableListOf<RecipeCard>()
+    for(recipe in recipesListJson)
+        recipeCards.add(Json.decodeFromString(recipe))
+    return recipeCards
 }
