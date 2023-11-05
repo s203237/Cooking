897b46c8cd0dd3d72e69ccb73f97311dc0c7b1de
Melissa Woo <s224311@dtu.dk>
2023-11-05 22:46:01 +0100

    adding a GET post to fetch recipe by id

--- app/src/main/java/com/example/cooking/data/remote/ApiService.kt
+++ app/src/main/java/com/example/cooking/data/remote/ApiService.kt
@@ -1,6 +1,12 @@
 package com.example.cooking.data.remote
+import com.example.cooking.model.Recipe
 import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ApiService {
     @GET("collection/breakfast-recipes/recipes/?rapidapi-key=564d89b1demshec6f0e210cbdd20p1c252ajsnafddd033c068")
     suspend fun fetchRecipeCollection(): RecipeCollection
+
+    @GET("recipe/{recipeId}/?rapidapi-key=564d89b1demshec6f0e210cbdd20p1c252ajsnafddd033c068")
+    suspend fun fetchRecipeById(@Path("recipeId") recipeID: String): Recipe
 }
