2fb925006ce9a1b98b10cf2fe9fd6dc0994979c7
Melissa Woo <s224311@dtu.dk>
2023-11-05 11:50:33 +0100

    updating image url field name

--- app/src/main/java/com/example/cooking/model/Recipe.kt
+++ app/src/main/java/com/example/cooking/model/Recipe.kt
@@ -1,10 +1,11 @@
 package com.example.cooking.model
 
 import androidx.annotation.DrawableRes
-
+import kotlinx.serialization.Serializable
+@Serializable
 data class Recipe(
     val title: String,
-    @DrawableRes val mainImage: Int,
+    @DrawableRes val imageUrl: Int,
     val imageDescription: String,
     val author: String,
     val prepTime: Int,
