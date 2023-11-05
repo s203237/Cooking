4cd8a150121d3eabb497f90e61185181861546b2
Melissa Woo <s224311@dtu.dk>
2023-11-05 11:48:13 +0100

    temporarily changing dependency provider to MockRecipeCardDataSource

--- app/src/main/java/com/example/cooking/DependencyProvider.kt
+++ app/src/main/java/com/example/cooking/DependencyProvider.kt
@@ -1,15 +1,16 @@
 package com.example.cooking
 
-import com.example.cooking.data.remote.MockRecipeDataSource
+import com.example.cooking.data.remote.MockRecipeCardDataSource
+//import com.example.cooking.data.remote.MockRecipeDataSource
 import com.example.cooking.data.remote.RecipeDataSource
 
 object DependencyProvider {
-    val recipeDataSource: RecipeDataSource = MockRecipeDataSource()
+    val recipeDataSource: RecipeDataSource = MockRecipeCardDataSource()
 }
 
 /* NOTE ON DEPENDENCY PROVIDER
 A dependency provider is used in order for our code to follow the Dependency Injection
 principle. This allows the ViewModel to get the data it needs through the Dependency Provider instead
-of fetching it directly. This allows for lower coupling and increased code reusability as it
+of fetching it directly. This allows for lower coupling and increased code re-usability as it
 can be used to provide different dependencies throughout the app
  */
