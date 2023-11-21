d61f0e7af0d8066790ba293278e8f34e8f4a6fa4
Melissa Woo <s224311@dtu.dk>
2023-11-21 10:49:44 +0100

    cleaning up custom colors

--- app/src/main/java/com/example/cooking/UI/theme/Color.kt
+++ app/src/main/java/com/example/cooking/UI/theme/Color.kt
@@ -11,10 +11,3 @@ val Blue = Color(0xFF93BBCF)
 val LightBlue = Color(0xFFC1DAE2)
 val Maroon = Color(0xFF7D5260)
 val Black = Color(0xFF000000)
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
