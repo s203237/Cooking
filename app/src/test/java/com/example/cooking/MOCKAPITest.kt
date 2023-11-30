4e8268b5ca99fabce3a17e311dc6c0595c6c4a40
Magnus Ottosen <magnus@ottosen.dk>
2023-11-30 20:46:57 +0100

    Update MOCKAPITest.kt

--- app/src/test/java/com/example/cooking/MOCKAPITest.kt
+++ app/src/test/java/com/example/cooking/MOCKAPITest.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.cooking.UI.Homepage.HomePageViewModel
 import com.example.cooking.model.RecipeCard
+import com.example.cooking.model.Recipe
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
@@ -18,7 +19,7 @@ import org.junit.Assert.assertEquals
 
 class MOCKAPITest {
 
-    suspend fun getDataAPI(): List<RecipeCard>{
+    suspend fun getDataCollectionAPI(): List<RecipeCard>{
         val recipeCards = DependencyProvider.recipeCardRepo.fetchData("High-protein-vegan-meals")
         return recipeCards
     }
@@ -26,12 +27,28 @@ class MOCKAPITest {
     @Test
     fun MockAPI_Results() = runTest{
 
-        val recipeCards = getDataAPI()
+        val recipeCards = getDataCollectionAPI()
 
         assertEquals("veggie-protein-chilli", recipeCards.get(0).recipeId)
         assertEquals("Veggie protein chilli", recipeCards.get(0).title)
         assertEquals("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/protein-veggie-chilli-6395abf.jpg", recipeCards.get(0).imageUrl)
     }
+
+    suspend fun getDataRecipe(): Recipe{
+        val recipeData = DependencyProvider.recipeRepo.fetchData("samphire-crab-salad")
+        return recipeData
+    }
+
+    @Test
+    fun API_recipe() = runTest {
+
+        val recipe = getDataRecipe()
+
+        assertEquals("Samphire & crab salad", recipe.title)
+        assertEquals("https://images.immediate.co.uk/production/volatile/sites/30/2022/04/Samphire-and-crab-salad-080df73.jpg", recipe.imageUrl)
+        assertEquals("Enjoy this salad as a generous dinner for two, or as a light lunch or supper for four. Samphire sings when paired with crab and this zesty dressing", recipe.recipeDescription)
+    }
+
 }
 /*
 {
