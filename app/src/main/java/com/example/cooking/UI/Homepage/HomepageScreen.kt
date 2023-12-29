7107f991aa02f5cbea8ffbae896bf64883f59553
Melissa Woo <s224311@dtu.dk>
2023-12-29 20:31:13 +0100

    clean up unused code

--- app/src/main/java/com/example/cooking/UI/Homepage/HomepageScreen.kt
+++ app/src/main/java/com/example/cooking/UI/Homepage/HomepageScreen.kt
@@ -36,39 +36,6 @@ fun HomepageScreen(onNavigateToRecipe: (String) -> Unit){
     collections.forEach { coll ->
         Log.v("HomepageScreen", coll.collectionName)
     }
-    //val dailyRecipe by viewModel.dailyRecipe.collectAsState()
-
-    //val dailyRecipe = RecipeData().loadRecipes()[0]
-
-  /*  val recipeList1 by viewModel.recipeCards1.collectAsState()
-    val recipeList2 by viewModel.recipeCards2.collectAsState()
-    val recipeList3 by viewModel.recipeCards3.collectAsState()
-    val recipeList4 by viewModel.recipeCards4.collectAsState()
-
-    val food1 = FoodCategories(categoryName = "high protein vegan recipes", categoryListOfRecipe = recipeList1)
-    val food2 = FoodCategories(categoryName = "vegan winter recipes", categoryListOfRecipe = recipeList2)
-    val food3 = FoodCategories(categoryName = "vegan lentil recipes", categoryListOfRecipe = recipeList3)
-    val food4 = FoodCategories(categoryName = "vegan slow cooker recipes", categoryListOfRecipe = recipeList4)
-
-
-    val listOfList: List<FoodCategories> = listOf(
-        food1, food2, food3, food4
-    )
-
-    val randomNumber = (0..3).random()
-
-    var list = food1.getList()
-
-    when (randomNumber) {
-        0 -> list = food1.getList()
-        1 -> list = food2.getList()
-        2 -> list = food3.getList()
-        else -> list = food4.getList()
-    }
-
-    if(list.size > 0) {
-        val dailyRecipe = list[Random.nextInt(list.size)]
-*/
 
     scrollableList(
             modifier = Modifier
