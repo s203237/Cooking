14032afdb8405a4cbbbb1d87452b8bd0bfd7c11c
NegarOstad <s224283@student.dtu.dk>
2024-01-11 14:35:26 +0100

    favorite and un favorite

--- app/src/main/java/com/example/cooking/model/RecipeCollection.kt
+++ app/src/main/java/com/example/cooking/model/RecipeCollection.kt
@@ -29,7 +29,22 @@ data class RecipeCard(
     val name: String = "defaultName",
     val thumbnail_url: String = "",
     var isFavorite: Boolean = false
-)
+){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RecipeCard
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
 
 
 /*@Serializable
