58a5e178a57cd3425c146d1a107fb0e38ccb81df
NegarOstad <s224283@student.dtu.dk>
2024-01-15 10:45:54 +0100

    add text to favorite page

--- app/src/main/java/com/example/cooking/UI/Favorite/FavoritesScreen.kt
+++ app/src/main/java/com/example/cooking/UI/Favorite/FavoritesScreen.kt
@@ -5,17 +5,30 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.cooking.UI.RecipeList.RecipeList
 
 @Composable
 fun FavoritesScreen(onNavigateToRecipe:(Int)->Unit) {
 
+    Text(
+        text = "Favorites:",
+        fontSize = 22.sp,
+        fontWeight = FontWeight.ExtraBold,
+        textAlign = TextAlign.Right
+
+
+    )
     val favoritesViewModel: FavoritesScreenViewModel = viewModel()
 
    /* LaunchedEffect(key1 = collectionName){
@@ -31,9 +44,10 @@ fun FavoritesScreen(onNavigateToRecipe:(Int)->Unit) {
         onNavigateToRecipe = onNavigateToRecipe,
         onFavoriteButtonClicked = favoritesViewModel::onFavoriteButtonClicked,
         modifier = Modifier
-            .padding(16.dp)
+            .padding(20.dp)
             .background(color = MaterialTheme.colorScheme.background)
             .fillMaxWidth()
+            
     )
     Log.d("FavoritesScreen", "Collected Recipes: $recipes")
 
